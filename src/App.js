42c6afb6126cc7ee0800880268e3ce801b7b58ea
github username <you@example.com>
2020-07-01 01:53:10 -0400

    add componentDidMount method to App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  return (
+
+  componentDidMount() {
+    //request info from this resource, it is GET method by default
+      fetch('http://localhost:3000/lists/1')
+    // return value of the above request is passed as argument to function below and converted to json
+      .then(resp => resp.json())
+    // return value of the above function is a json version of the response, it is passed as argument to function below
+    // we will never go the this function until the response is converted to json
+      .then(data => console.log(data))
+  }
+
+  render() {
+   return (
     <div className="App">
       App
     </div>
-  );
+    );
+  }
 }
 
 export default App;
